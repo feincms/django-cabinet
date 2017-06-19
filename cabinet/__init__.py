06fd808cd3802dfb4bf6cd59923665b9c4df3c72
Matthias Kestenholz <mk@feinheit.ch>
2017-06-19 11:14:07 +0200

    Add a nice title

--- cabinet/__init__.py
+++ cabinet/__init__.py
@@ -1,2 +1,4 @@
 VERSION = (0, 0, 0)
 __version__ = '.'.join(map(str, VERSION))
+
+default_app_config = 'cabinet.apps.CabinetConfig'
---
+++ cabinet/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class CabinetConfig(AppConfig):
+    name = 'cabinet'
+    verbose_name = _('Cabinet media library')
