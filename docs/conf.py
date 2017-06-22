f38df3951412d33899c2ac9d1ed95bb0fafd1078
Matthias Kestenholz <mk@feinheit.ch>
2017-06-22 08:49:11 +0200

    Merge .gitignore files, update the Sphinx configuration files

--- docs/conf.py
+++ docs/conf.py
@@ -1,54 +1,57 @@
 from __future__ import unicode_literals
 
 import os
+import re
 import sys
 
 sys.path.append(os.path.abspath('..'))
 
-extensions = []
-
-templates_path = ['_templates']
-
-source_suffix = '.rst'
-
-master_doc = 'index'
-
 project = 'django-cabinet'
-copyright = '2017 Feinheit AG'
-
+author = 'Feinheit AG'
+copyright = '2017,' + author
 version = __import__('cabinet').__version__
 release = version
+language = 'en'
 
+#######################################
+project_slug = re.sub(r'[^a-z]+', '', project)
+
+extensions = []
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
+todo_include_todos = False
 
 html_theme = 'alabaster'
-
 html_static_path = ['_static']
+htmlhelp_basename = project_slug + 'doc'
 
-htmlhelp_basename = 'djangocabinetdoc'
-
+latex_elements = {
+    'papersize': 'a4',
+}
 latex_documents = [(
-    'index',
-    'djangocabinet.tex',
-    'django-cabinet Documentation',
-    'Feinheit AG',
+    master_doc,
+    project_slug + '.tex',
+    project + ' Documentation',
+    author,
     'manual',
 )]
-
 man_pages = [(
-    'index',
-    'djangocabinet',
-    'django-cabinet Documentation',
-    ['Feinheit AG'],
+    master_doc,
+    project_slug,
+    project + ' Documentation',
+    [author],
     1,
 )]
-
 texinfo_documents = [(
-    'index',
-    'djangocabinet',
-    'django-cabinet Documentation',
-    'Feinheit AG',
-    'djangocabinet',
-    'Authentication utilities for Django',
+    master_doc,
+    project_slug,
+    project + ' Documentation',
+    author,
+    project_slug,
+    '',  # Description
     'Miscellaneous',
 )]
