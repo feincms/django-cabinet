1cbe16d4fb1e77063b85d95b90ddb48b23f27ab4
Matthias Kestenholz <mk@feinheit.ch>
2020-11-24 11:55:01 +0100

    Switch to using a declarative setup

--- setup.py
+++ setup.py
@@ -1,38 +1,5 @@
-#!/usr/bin/env python
-
-import os
-
-from setuptools import find_packages, setup
+#!/usr/bin/env python3
+from setuptools import setup
 
 
-def read(filename):
-    return open(os.path.join(os.path.dirname(__file__), filename)).read()
-
-
-setup(
-    name="django-cabinet",
-    version=__import__("cabinet").__version__,
-    description="Media library for Django",
-    long_description=read("README.rst"),
-    author="Matthias Kestenholz",
-    author_email="mk@feinheit.ch",
-    url="http://github.com/matthiask/django-cabinet/",
-    license="BSD License",
-    platforms=["OS Independent"],
-    packages=find_packages(exclude=["tests"]),
-    include_package_data=True,
-    install_requires=["django-imagefield", "django-tree-queries"],
-    classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-    ],
-    zip_safe=False,
-)
+setup()
