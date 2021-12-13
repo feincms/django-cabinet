113ec1131ed23f7479dd12d764a537e8d9bf602b
Matthias Kestenholz <mk@feinheit.ch>
2021-12-13 15:22:34 +0100

    Do not fail when Django isn't there (when building)

--- cabinet/__init__.py
+++ cabinet/__init__.py
@@ -1,7 +1,10 @@
-import django
-
 VERSION = (0, 11, 5)
 __version__ = ".".join(map(str, VERSION))
 
-if django.VERSION < (3, 2):
-    default_app_config = "cabinet.apps.CabinetConfig"
+try:
+    import django
+except ModuleNotFoundError:
+    pass
+else:
+    if django.VERSION < (3, 2):
+        default_app_config = "cabinet.apps.CabinetConfig"
